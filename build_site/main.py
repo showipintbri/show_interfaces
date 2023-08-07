b471a4e50395ba57c2089422e837710942cd9cc3
showipintbri <40892800+showipintbri@users.noreply.github.com>
2023-08-06 21:56:31 -0400

    inner html

--- build_site/main.py
+++ build_site/main.py
@@ -1,5 +1,6 @@
 from find_id_file import FindIdFile
 from build_folder_structure import BuildFoldersAndFiles
+import sys
 
 
 # remember include the leading "." infront of the extension letters
@@ -27,17 +28,32 @@ def build_infrastructure(file_text):
     bfaf.make_dirs_from_tags(file_path, just_tags_text_list, filenames)
     return None
 
-def build_site(file_text):
+def build_inner_html(file_text):
     bfaf = BuildFoldersAndFiles()
     all_start_tags_list = bfaf.find_all_start_tags(file_text)
     just_tags_text_list = bfaf.extract_all_tag_text(all_start_tags_list)
     list_of_dicts = bfaf.get_tag_and_element(file_text, just_tags_text_list)
-    print(list_of_dicts)
-    return None
+    # print(list_of_dicts)
+    for i in list_of_dicts:
+        tag = i['tag']
+        element = i['element']
+        old_line = f'<{tag}>{element}</{tag}>'
+        new_line = f'<span class="element" id="{tag}">{element}</span>'
+        file_text = file_text.replace(old_line, new_line)
+    # print(file_text)
+    list_o_lines = file_text.splitlines()
+    text_block = ''
+    for x in list_o_lines:
+        old_line = x
+        new_line = f'<div>{old_line}</div>\n'
+        text_block =+ new_line
+    return text_block
 
 build_infrastructure(file_text)
 
-build_site(file_text)
+inner_html = build_inner_html(file_text)
+
+print(inner_html)
 
 sys.exit(0)
 
